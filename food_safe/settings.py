6e6828224c398630807cdff6d473309ef9a9fb35
Jesse <jesse.wimer@wsu.edu>
2025-07-12 13:50:40 -0700

    Add STATIC_ROOT and STATICFILES_DIRS

--- food_safe/settings.py
+++ food_safe/settings.py
@@ -117,6 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = '/home/foodsafe/food-safe/static/'
+
+# If you have app-specific static files
+STATICFILES_DIRS = [
+    BASE_DIR / "shelf_life" / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
