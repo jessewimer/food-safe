f2174a49d1e280ba1f0fa79739610a6d45f05386
Jesse <jesse.wimer@wsu.edu>
2025-07-12 12:49:07 -0700

    Remove CustomUser

--- users/models.py
+++ users/models.py
@@ -1,8 +1,2 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
 
-class CustomUser(AbstractUser):
-    organization_name = models.CharField(max_length=255)
-    
-    def __str__(self):
-        return self.username
+from django.db import models
